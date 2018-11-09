b13ef169d14facf5a37814cc8669ed30c8c87e6e
phapdinh <centralparkloop@yahoo.com>
2018-11-09 00:09:37 -0500

    move bootstrap project in

--- app.js
+++ app.js
@@ -8,46 +8,54 @@ server.connection({ port: 3000});
 server.register(require('inert'), function(err) {
 
 	    if (err) {
-		            throw err;
-		        }
+			throw err;
+		}
 
 	    server.route({
-		            method: 'GET',
-		            path: '/js/{file*}',
-		            handler: {
-				                directory: {
-							                path: 'js'
-							            }
-				            }
-		        });
+		    method: 'GET',
+			path: '/js/{file*}',
+		    handler: {
+				directory: {
+					path: 'js'
+				}
+			}
+		});
 
 	    server.route({
-		            method: 'GET',
-		            path: '/css/{file*}',
-		            handler: {
-				                directory: {
-							                path: 'css'
-							            }
-				            }
-		        });
+		    method: 'GET',
+		    path: '/css/{file*}',
+		    handler: {
+				directory: {
+					path: 'css'
+				}
+			}
+		});
 
 	    server.route({
-		            method: 'GET',
-		            path: '/images/{file*}',
-		            handler: {
-				                directory: {
-							                path: 'images'
-							            }
-				            }
-		        });
+		    method: 'GET',
+		    path: '/images/{file*}',
+		    handler: {
+				directory: {
+					path: 'images'
+				}
+			}
+		});
 
 	    server.route({
-		            method: 'GET',
-		            path: '/',
-		            handler: function (request, reply) {
-				                reply.file('./index.html');
-				            }
-		        });
+		    method: 'GET',
+		    path: '/',
+		    handler: function (request, reply) {
+				reply.file('./index.html');
+			}
+		});
+
+		server.route({
+			method: 'GET',
+			path: '/bootstrap',
+			handler: function (request, reply) {
+				reply.file('./indexBootstrap.html');
+			}
+		});
 	});
 
 
